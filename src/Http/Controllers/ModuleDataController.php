5fe89c15415ae9d178798cc744f40b61a152ea33
Jeroenverloop <10675507+Jeroenverloop@users.noreply.github.com>
2025-07-23 21:15:23 +0000

    Fix styling

--- src/Http/Controllers/ModuleDataController.php
+++ src/Http/Controllers/ModuleDataController.php
@@ -4,19 +4,16 @@ namespace Jeroenv\EcdModuleInstaller\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use function Pest\Laravel\json;
 
 class ModuleDataController extends Controller
 {
+    public function getModuleData(Request $request)
+    {
 
-    public function getModuleData(Request $request){
-
-
-        $data = new \stdClass();
+        $data = new \stdClass;
         $data->name = 'Client module';
 
         return json_encode($data);
 
     }
-
 }
