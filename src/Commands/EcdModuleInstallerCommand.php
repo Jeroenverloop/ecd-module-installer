7eb0bbba8f5698cb8658d0679e729328b3d7c897
LAPTOP-O8VD28IA\jeroe <jeroenverloop@gmail.com>
2025-07-25 01:13:30 +0200

    Command

--- src/Commands/EcdModuleInstallerCommand.php
+++ src/Commands/EcdModuleInstallerCommand.php
@@ -3,15 +3,18 @@
 namespace Jeroenv\EcdModuleInstaller\Commands;
 
 use Illuminate\Console\Command;
+use Jeroenv\EcdModuleInstaller\ExtendedComposer\Composer;
 
 class EcdModuleInstallerCommand extends Command
 {
-    public $signature = 'ecd-module-installer';
+    public $signature = 'ecd:test-composer';
 
     public $description = 'My command';
 
     public function handle(): int
     {
+        app()->make(Composer::class)->run(['update']);
+
         $this->comment('All done');
 
         return self::SUCCESS;
--- src/Http/Controllers/ModuleDataController.php
+++ src/Http/Controllers/ModuleDataController.php
@@ -4,6 +4,7 @@ namespace Jeroenv\EcdModuleInstaller\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Jeroenv\EcdModuleInstaller\ExtendedComposer\Composer;
 
 class ModuleDataController extends Controller
 {
@@ -12,6 +13,9 @@ class ModuleDataController extends Controller
 
         $data = new \stdClass;
         $data->name = 'Client module';
+
+
+        app()->make(Composer::class)->run(['require', 'some-package']);
 
         return json_encode($data);
 
